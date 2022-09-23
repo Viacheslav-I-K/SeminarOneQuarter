1a166798d05474a5503e92ae9fa61caec9053d29
Viacheslav <Animal26@yandex.ru>
2022-09-23 19:54:38 +0300

    add Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+/*
+Задача 13. Напишите программу, которая выводит
+третью цифру заданного числа или сообщает, что
+третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+
+Console.WriteLine("Введите число большее 99: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumberValencyThree(int num)
+{
+    //int numVal = num;
+    while (num / 1000 >= 1)
+    {
+        num = num / 10;
+    }
+    int res = num % 10;
+    return res;
+}
+
+if (number < 100 && number > -100)
+{
+    Console.WriteLine($"В числе {number} третьей цифры нет.");
+    
+}
+else
+{
+int result = NumberValencyThree(number);
+    if (result > 0)
+    {
+        Console.WriteLine($"Третья цифра числа {number}: {result}");
+    }
+    else
+    {
+       Console.WriteLine($"Третья цифра числа {number}: {result * -1}"); 
+    }
+
+}
+
+
