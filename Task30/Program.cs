f2a9fe6caf588447ec430f71ed371ad1679a3946
Viacheslav <Animal26@yandex.ru>
2022-09-28 19:33:39 +0300

    add Task 30

---
+++ Task30/Program.cs
@@ -0,0 +1,35 @@
+﻿
+/*
+Задача 30: Напишите программу, которая
+выводит массив из 8 элементов, заполненный
+нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+Console.WriteLine("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[sizeArray];
+
+void RandomNumbers(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        arr[i] = rnd.Next(0, 2);   
+    }
+}
+
+RandomNumbers(array);
+PrintArray(array);
+
+void PrintArray(int[] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write($"[{arr[i]},");
+        if (i > 0 && i < arr.Length - 1) Console.Write($"{arr[i]},");
+        if (i == arr.Length - 1) Console.Write($"{arr[i]}" + "]");
+    }
+}
+
