99436442b7f1e907252e511fe65ebb9c10534a03
Viacheslav <Animal26@yandex.ru>
2022-10-06 21:09:10 +0300

    add Task 39

---
+++ Task39/Program.cs
@@ -0,0 +1,83 @@
+﻿
+/*
+Задача 39: Напишите программу, которая перевернёт
+одномерный массив (последний элемент будет на первом
+месте, а первый - на последнем и т.д.)
+[1 2 3 4 5] -> [5 4 3 2 1]
+[6 7 3 6] -> [6 3 7 6]
+*/
+
+Console.Write("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное число в массиве: ");
+int numberMin = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное число в массиве: ");
+int numberMax = Convert.ToInt32(Console.ReadLine());
+
+
+int[] CreateArrayRndInt(int size, int numMin, int numMax)
+{
+    Random rnd = new Random();
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(numMin, numMax + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.WriteLine($"{array[i]}]");
+    }
+}
+
+void ReverseArray(int[] array)// через цикл while
+{
+    int size = array.Length;
+    int index1 = 0;
+    int index2 = size - 1;
+
+    while (index1 < index2)
+    {
+        int obj = array[index1];
+        array[index1] = array[index2];
+        array[index2] = obj;
+
+        index1++;
+        index2--;
+    }
+
+}
+
+void ReverseArray2(int[] array)// через цикл for
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size / 2; i++)
+    {
+        int obj = array[i];
+        array[i] = array[size - 1 - i];
+        array[size - 1 - i] = obj;
+
+    }
+
+}
+
+int[] arr = CreateArrayRndInt(sizeArray, numberMin, numberMax);
+PrintArray(arr);
+ReverseArray(arr);
+PrintArray(arr);
+Array.Reverse(arr);
+PrintArray(arr);
+ReverseArray2(arr);
+PrintArray(arr);
+
+
