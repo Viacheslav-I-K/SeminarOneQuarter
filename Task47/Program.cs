0bab57dcb011dea2d28add9a691355de39e5ec1e
Viacheslav <Animal26@yandex.ru>
2022-10-08 19:46:28 +0300

    add Task 47

---
+++ Task47/Program.cs
@@ -0,0 +1,59 @@
+﻿
+/*
+Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+
+Console.Write("Введите количество строк в 2D массиве: ");
+int sizeRows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов в 2D массиве: ");
+int sizeColumns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное число элемента массива: ");
+int minNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное число элемента массива: ");
+int maxNumber = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrixDouble = CreaterMatrixRndDouble(sizeRows, sizeColumns, minNumber, maxNumber);
+PrintMatrix(matrixDouble);
+
+
+
+double[,] CreaterMatrixRndDouble(int rows, int columns, double min, double max)
+{
+    Random rnd = new Random();
+    double[,] matrix = new double[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * ((max - min) + min), 1, MidpointRounding.ToZero);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4} | ");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+
