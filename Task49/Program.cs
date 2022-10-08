ee3df7615b3728ee8fa41081789d25f7a72934b2
Viacheslav <Animal26@yandex.ru>
2022-10-08 18:18:02 +0300

    add Task 49

---
+++ Task49/Program.cs
@@ -0,0 +1,77 @@
+﻿
+/*
+Задача 49: Задайте двумерный массив. Найдите элементы, у
+которых оба индекса чётные, и замените эти элементы на их
+квадраты.
+*/
+
+
+Console.Write("Введите количество строк в 2D массиве: ");
+int sizeRows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов в 2D массиве: ");
+int sizeColumns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное число элемента массива: ");
+int minNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное число элемента массива: ");
+int maxNumber = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] array2D = CreaterMatrixRndInt(sizeRows, sizeColumns, minNumber, maxNumber);
+Console.WriteLine("Первичный массив:");
+PrintMatrix(array2D);
+
+Console.WriteLine();
+
+int[,] newArray2D = CreaterMatrixSqrNumByIndex(array2D, sizeRows, sizeColumns);
+Console.WriteLine("Получившийся массив:");
+PrintMatrix(newArray2D);
+
+
+int[,] CreaterMatrixRndInt(int rows, int columns, int min, int max)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4} | ");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] CreaterMatrixSqrNumByIndex(int[,] matrix, int rows, int columns)
+{
+    int[,] newMatrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) newMatrix[i, j] = matrix[i, j] * matrix[i, j];
+            else newMatrix[i, j] = matrix[i, j];
+        }
+    }
+    return newMatrix;
+}
+
+
+
+
