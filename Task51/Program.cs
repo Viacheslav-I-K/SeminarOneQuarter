50f765cddfea5c48746916f31901b435304ad37e
Viacheslav <Animal26@yandex.ru>
2022-10-08 18:30:47 +0300

    add Task 51

---
+++ Task51/Program.cs
@@ -0,0 +1,70 @@
+﻿
+/*
+Задача 51: Задайте двумерный массив. Найдите сумму
+элементов, находящихся на главной диагонали (с индексами
+(0,0); (1;1) и т.д.
+*/
+
+Console.Write("Введите количество строк в 2D массиве: ");
+int sizeRows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов в 2D массиве: ");
+int sizeColumns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное число элемента массива: ");
+int minNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное число элемента массива: ");
+int maxNumber = Convert.ToInt32(Console.ReadLine());
+
+int[,] array2D = CreaterMatrixRndInt(sizeRows, sizeColumns, minNumber, maxNumber);
+Console.WriteLine("Первичный массив:");
+PrintMatrix(array2D);
+
+Console.WriteLine();
+
+int sumElem = SumElemMainDiagonal(array2D);
+Console.Write($"Сумма элементов, находящихся на главной диагонали = {sumElem}.");
+
+
+int[,] CreaterMatrixRndInt(int rows, int columns, int min, int max)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4} | ");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int SumElemMainDiagonal(int[,] matrix)
+{
+    int result = default;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) result += matrix[i, j];
+        }
+    }
+    return result;
+}
+
