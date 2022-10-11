633cf8e199abfde0c0d6e18f7dab6c754acb73ee
Viacheslav <Animal26@yandex.ru>
2022-10-11 14:07:24 +0300

    add Task 57

---
+++ Task57/Program.cs
@@ -0,0 +1,110 @@
+﻿
+/*
+Задача 57: Составить частотный словарь элементов
+двумерного массива. Частотный словарь содержит
+информацию о том, сколько раз встречается элемент
+входных данных.
+*/
+
+
+Console.Write("Введите количество строк в 2D массиве: ");
+int sizeRows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов в 2D массиве: ");
+int sizeColumns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное число элемента массива: ");
+int minNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное число элемента массива: ");
+int maxNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] matrix2D = CreaterMatrixRndInt(sizeRows, sizeColumns, minNumber, maxNumber);
+PrintMatrix(matrix2D);
+
+Console.WriteLine();
+
+dictStat(matrix2D);
+
+
+
+
+
+
+int[,] CreaterMatrixRndInt(int rows, int columns, int min, int max)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4} | ");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int MaxMatrix(int[,] matrix)
+{
+    int MaxResult = matrix[0, 0];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] > MaxResult) MaxResult = matrix[i, j];
+        }
+    }
+    return MaxResult;
+}
+
+int MinMatrix(int[,] matrix)
+{
+    int MinResult = matrix[0, 0];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < MinResult) MinResult = matrix[i, j];
+        }
+    }
+    return MinResult;
+}
+
+void dictStat(int[,] matrix)
+{
+
+    int maxValue = MaxMatrix(matrix);
+    int minValue = MinMatrix(matrix);
+    for (int value = minValue; value <= maxValue; value++)
+    {
+    int result = default;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i,j] == value) result++;
+        }
+    }
+    if (result!=0) Console.WriteLine($"Число {value} встречается {result} раз");
+    }
+}
+
+
+
+
